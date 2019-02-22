54fd8e7ff138e5fba2d4b9cfcb22abe5d7d84fe0
Filip Szymaniak <fszymaniak@future-processing.com>
2019-02-22 17:00:52 +0100

    Some changes, specflow feature.cs is not working

--- OrangeHRM.App.UI.Tests/Features/LoginLogout.feature.cs
+++ OrangeHRM.App.UI.Tests/Features/LoginLogout.feature.cs
@@ -1,26 +1,17 @@
-﻿We could not find a data exchange file at the path System.Configuration.ConfigurationErrorsException: SpecFlow configuration error ---> System.Xml.XmlException: Data at the root level is invalid. Line 1, position 1.
+﻿We could not find a data exchange file at the path Unhandled Exception: System.BadImageFormatException: The module was expected to contain an assembly manifest. (Exception from HRESULT: 0x80131018)
 
 Please open an issue at https://github.com/techtalk/SpecFlow/issues/
 Complete output: 
-System.Configuration.ConfigurationErrorsException: SpecFlow configuration error ---> System.Xml.XmlException: Data at the root level is invalid. Line 1, position 1.
-   at System.Xml.XmlTextReaderImpl.Throw(Exception e)
-   at System.Xml.XmlTextReaderImpl.Throw(String res, String arg)
-   at System.Xml.XmlTextReaderImpl.ParseRootLevelWhitespace()
-   at System.Xml.XmlTextReaderImpl.ParseDocumentContent()
-   at System.Xml.XmlTextReaderImpl.Read()
-   at System.Xml.XmlTextReader.Read()
-   at System.Configuration.ConfigurationSection.DeserializeSection(XmlReader reader)
-   at TechTalk.SpecFlow.Configuration.ConfigurationSectionHandler.CreateFromXml(String xmlContent)
-   at TechTalk.SpecFlow.Generator.Configuration.GeneratorConfigurationProvider.GetPlugins(SpecFlowConfigurationHolder configurationHolder)
-   --- End of inner exception stack trace ---
-   at TechTalk.SpecFlow.Generator.Configuration.GeneratorConfigurationProvider.GetPlugins(SpecFlowConfigurationHolder configurationHolder)
-   at TechTalk.SpecFlow.Generator.GeneratorContainerBuilder.LoadPlugins(ObjectContainer container, IGeneratorConfigurationProvider configurationProvider, SpecFlowConfigurationHolder configurationHolder)
-   at TechTalk.SpecFlow.Generator.GeneratorContainerBuilder.CreateContainer(SpecFlowConfigurationHolder configurationHolder, ProjectSettings projectSettings)
-   at TechTalk.SpecFlow.Generator.TestGeneratorFactory.CreateGenerator(ProjectSettings projectSettings)
-   at TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.Actions.GenerateTestFileAction.GenerateTestFile(GenerateTestFileParameters opts)
+
+Unhandled Exception: System.BadImageFormatException: The module was expected to contain an assembly manifest. (Exception from HRESULT: 0x80131018)
+   at System.Reflection.RuntimeAssembly.nLoadFile(String path, Evidence evidence)
+   at System.Reflection.Assembly.LoadFile(String path)
+   at TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.CommandLineHandling.PreLoadAssemblies()
+   at TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.CommandLineHandling.PreLoadAssemblies()
+   at TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.Program.Main(String[] args)
 
 
 
 Command: C:\Users\fszymaniak\AppData\Local\Microsoft\VisualStudio\15.0_36a34d3a\Extensions\0pcxkurm.0hv\TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.exe
-Parameters: GenerateTestFile --featurefile C:\Users\fszymaniak\AppData\Local\Temp\tmp7C67.tmp --outputdirectory C:\Users\fszymaniak\AppData\Local\Temp --projectsettingsfile C:\Users\fszymaniak\AppData\Local\Temp\tmp7C66.tmp
+Parameters: GenerateTestFile --featurefile C:\Users\fszymaniak\AppData\Local\Temp\tmp34E8.tmp --outputdirectory C:\Users\fszymaniak\AppData\Local\Temp --projectsettingsfile C:\Users\fszymaniak\AppData\Local\Temp\tmp34E7.tmp
 Working Directory: 
